c66384bf01a8f83539ccfc0e41e54c51aef55c46
Roland König <roland.koenig@rolandk.de>
2025-02-07 19:21:23 +0800

    Fixed macOS package naming (#4)
    * Updated macOS package name
    
    * Updated executable name

--- scripts/publish-macos.ps1
+++ scripts/publish-macos.ps1
@@ -5,4 +5,4 @@ mkdir ../publish
 # Create macOS app package
 dotnet publish -c Release -r osx-arm64 --self-contained -o ../publish/GpxViewer2/Contents/MacOS -p:PublishReadyToRun=true ../src/GpxViewer2/GpxViewer2.csproj
 cp -r ../macos-app-template/* ../publish/GpxViewer2/Contents
-mv ../publish/GpxViewer2 ../publish/GpxViewer2.app
+mv ../publish/GpxViewer2 "../publish/RolandK GpxViewer 2.app"
