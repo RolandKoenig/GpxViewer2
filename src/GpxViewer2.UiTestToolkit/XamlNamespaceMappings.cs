8d66f60b4bf0bb21cb3186bcdb61d4f1d1d39fd0
Roland König <roland.koenig@rolandk.de>
2025-10-19 17:45:02 +0200

    optimize xaml namespaces (#53)
    * Updated namespace of the local application
    
    * Updated XAML-Namespace of TestToolkit

--- src/GpxViewer2.UiTestToolkit/XamlNamespaceMappings.cs
+++ src/GpxViewer2.UiTestToolkit/XamlNamespaceMappings.cs
@@ -1,5 +1,6 @@
 using Avalonia.Metadata;
 
+[assembly: XmlnsPrefix("https://github.com/RolandKUiTestToolkit", "t")]
 [assembly: XmlnsDefinition(
     "https://github.com/RolandKUiTestToolkit", 
     "GpxViewer2.UiTestToolkit")]
---
+++ src/GpxViewer2/XamlNamespaceMappings.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Metadata;
+
+// Common XAML namespace (things like MarkupExtensions, Converters, etc.)
+[assembly: XmlnsPrefix("local-application-common", "c")]
+[assembly: XmlnsDefinition(
+    "local-application-common",
+    "GpxViewer2.MarkupExtensions")]
+[assembly: XmlnsDefinition(
+    "local-application-common",
+    "GpxViewer2.Controls")]
+
+// View XAML namespace (for Views / ViewModels only)
+[assembly: XmlnsPrefix("local-application-views", "v")]
+[assembly: XmlnsDefinition(
+    "local-application-views",
+    "GpxViewer2")]
+[assembly: XmlnsDefinition(
+    "local-application-views",
+    "GpxViewer2.Views")]
+[assembly: XmlnsDefinition(
+    "local-application-views",
+    "GpxViewer2.Views.RouteSelection")]
